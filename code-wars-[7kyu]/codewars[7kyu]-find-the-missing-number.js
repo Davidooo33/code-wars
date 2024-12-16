b25c28e2e3d956f89f57a146c0109374aeefd90d
Davidooo33 <davidwright0121@gmail.com>
2024-12-16 11:27:56 +0000

    Create codewars[7kyu]-find-the-missing-number.js

---
+++ code-wars-[7kyu]/codewars[7kyu]-find-the-missing-number.js
@@ -0,0 +1,16 @@
+// This question is a variation on the Arithmetic Progression kata
+
+// The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+
+// You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+
+function missingNo(nums) {
+    const sortedNum = nums.sort(function (a, b) {  return a - b;  });
+    let answer
+    for (let i = 0; i < nums.length; i++) {
+      if ((sortedNum[i+1] - sortedNum[i]) === 2) {
+              answer = sortedNum[i] + 1
+      }
+    }
+     return answer
+  }
