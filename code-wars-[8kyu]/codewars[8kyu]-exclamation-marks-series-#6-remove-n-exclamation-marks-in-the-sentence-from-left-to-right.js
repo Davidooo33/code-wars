e7ca6728cb29a392f18043be223043292ef8741f
David J Wright <davidwright0121@gmail.com>
2025-03-03 23:38:48 +0000

    Create codewars[8kyu]-exclamation-marks-series-#6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right.js

---
+++ code-wars-[8kyu]/codewars[8kyu]-exclamation-marks-series-#6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right.js
@@ -0,0 +1,29 @@
+// Description:
+
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+// Examples
+
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+function remove(s,n){
+    //coding and coding....
+    let i = 0
+    let str = ''
+    while (i < s.length) {
+      if (s[i] == '!' && n > 0) {
+        n --
+      } else str += s[i]
+      i++
+    }
+    return str
+  }
